bb2fb8a443f021f01037ce1754d43ef05696d2fe
Yuanhao-Luo <1097308076@qq.com>
2022-10-03 12:06:12 +0100

    Home page components.

---
+++ movies/src/stories/movieListHeader.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MovieListHeader from "../components/headerMovieList";
+
+export default {
+    title: "Home Page/Header",
+    component: MovieListHeader,
+};
+
+export const Basic = () => < MovieListHeader title = { 'Discover Movies' }
+/>;
+
+Basic.storyName = "Default";
