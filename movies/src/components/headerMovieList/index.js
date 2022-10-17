5656a4817a3aeb8cef49b5a5199832e6c01cec34
Yuanhao-Luo <1097308076@qq.com>
2022-10-17 12:46:25 +0100

    Fix backward/forward arrows.

--- movies/src/components/headerMovieList/index.js
+++ movies/src/components/headerMovieList/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import Paper from "@mui/material/Paper";
@@ -7,6 +8,7 @@ import Typography from "@mui/material/Typography";
 
 const Header = (props ) => {
   const title = props.title
+  const navigate = useNavigate();
   return (
     <Paper 
       component="div" 
@@ -17,18 +19,14 @@ const Header = (props ) => {
         marginBottom: 1.5,
       }}
       >
-      <IconButton
-        aria-label="go back"
-      >
+      <IconButton aria-label="go back" onClick={() => navigate(-1)}>
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
 
       <Typography variant="h4" component="h3">
         {title}
       </Typography>
-      <IconButton
-        aria-label="go forward"
-      >
+      <IconButton aria-label="go forward" onClick={() => navigate(+1)}>
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
     </Paper>
--- movies/src/index.js
+++ movies/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Navigate, Routes} from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
