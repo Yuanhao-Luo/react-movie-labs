edd12bf862fbbb3d70d9dc3d43b3b99a4a1e85a1
Yuanhao-Luo <1097308076@qq.com>
2022-10-28 13:17:34 +0100

    New button for upcoming movies page.

--- movies/src/pages/upcomingPage.js
+++ movies/src/pages/upcomingPage.js
@@ -3,7 +3,7 @@ import { getUpcoming } from "../api/tmdb-api";
 import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from 'react-query';
 import Spinner from '../components/spinner';
-import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+import AddToPlayList from '../components/cardIcons/addToPlayList'
 
 const HomePage = (props) => {
 
@@ -28,7 +28,7 @@ const HomePage = (props) => {
       title="Upcoming Movies"
       movies={movies}
       action={(movie) => {
-        return <AddToFavoritesIcon movie={movie} />
+        return <AddToPlayList movie={movie} />
       }}
     />
   );
